b9152e8645db2f568b6c3de119fbc314018d2e25
Anton Ericson <antonericson97@gmail.com>
2021-12-07 10:15:03 +0100

    Added init files to init structure for a day

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ ! -d "$1" ]; then
+  mkdir $1
+    touch $1/input.txt $1/debug.txt $1/solution.py
+    echo "def main():
+    inputLines =  open(\"debug.txt\", \"r\").readlines()
+    
+    print(f'Part one: ')
+    print(f'Part two: ')
+
+if __name__ == \"__main__\":
+    main()" >> $1/solution.py
+else
+    echo "Files for day $1 already exist"
+fi
